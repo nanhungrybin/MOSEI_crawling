2af3757de809864a458f50e9e6f560aa551da784
nanhungrybin <vsos0642@naver.om>
2023-09-19 17:29:46 +0900

    first commit

---
+++ preprocess.py
@@ -0,0 +1,61 @@
+import cv2
+import os
+
+def process_ffmpeg(raw_vid_path, save_folder, save_vid_name,
+                   bbox, start_sec, end_sec):
+    """
+    raw_vid_path:
+    save_folder:
+    save_vid_name:
+    bbox: format: top, bottom, left, right. the values are normalized to 0~1
+    time: begin_sec, end_sec
+    """
+
+    def secs_to_timestr(secs):
+        hrs = secs // (60 * 60)
+        min = (secs - hrs * 3600) // 60 
+        sec = secs % 60
+        end = (secs - int(secs)) * 100
+        return "{:02d}:{:02d}:{:02d}.{:02d}".format(int(hrs), int(min),
+                                                    int(sec), int(end))
+
+    def expand(bbox, ratio):
+        top, bottom = max(bbox[0] - ratio, 0), min(bbox[1] + ratio, 1)
+        left, right = max(bbox[2] - ratio, 0), min(bbox[3] + ratio, 1)
+
+        return top, bottom, left, right
+
+    def to_square(bbox):
+        top, bottom, leftx, right = bbox
+        h = bottom - top
+        w = right - leftx
+        c = min(h, w) // 2
+        c_h = (top + bottom) / 2
+        c_w = (leftx + right) / 2
+
+        top, bottom = c_h - c, c_h + c
+        leftx, right = c_w - c, c_w + c
+        return top, bottom, leftx, right
+
+    def denorm(bbox, height, width):
+        top, bottom, left, right = \
+            round(bbox[0] * height), \
+            round(bbox[1] * height), \
+            round(bbox[2] * width), \
+            round(bbox[3] * width)
+
+        return top, bottom, left, right
+
+
+    out_path = os.path.join(save_folder, save_vid_name)
+
+    cap = cv2.VideoCapture(raw_vid_path)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    top, bottom, left, right = to_square(
+        denorm(expand(bbox, 0.02), height, width))
+    #start_sec, end_sec = time
+
+    #cmd = f"ffmpeg -i {raw_vid_path} -vf crop=w={right-left}:h={bottom-top}:x={left}:y={top} -ss {secs_to_timestr(start_sec)} -to {secs_to_timestr(end_sec)} -loglevel error {out_path}"
+    #os.system(cmd)
+    return out_path
